33d4acd819885716665216fcc5f2d0cda71daf28
MineMaster284 <jasoncollins818@gmail.com>
2024-03-25 09:29:36 -0400

    Remove unused messageCollector from NDCCompilerRegistrar

--- nullabledataclasses-plugin/src/main/kotlin/minerofmillions/nullabledataclasses/NDCCompilerRegistrar.kt
+++ nullabledataclasses-plugin/src/main/kotlin/minerofmillions/nullabledataclasses/NDCCompilerRegistrar.kt
@@ -20,8 +20,6 @@ package minerofmillions.nullabledataclasses
 
 import com.google.auto.service.AutoService
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
-import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
-import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import org.jetbrains.kotlin.config.CompilerConfiguration
@@ -31,8 +29,6 @@ class NDCCompilerRegistrar : CompilerPluginRegistrar() {
   override val supportsK2 = false
 
   override fun ExtensionStorage.registerExtensions(configuration: CompilerConfiguration) {
-    val messageCollector = configuration.get(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, MessageCollector.NONE)
-
     IrGenerationExtension.registerExtension(NDCIrGenerationExtension())
   }
 }
